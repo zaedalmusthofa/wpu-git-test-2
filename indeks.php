ed1a9ff223020d436a9354fd32fe671f4ca5834b
zaedalmusthofa <zaedalmusthofa123@gmail.com>
2024-01-31 15:33:30 +0700

    menambahkan paragraf

---
+++ indeks.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, temporibus. Amet necessitatibus architecto molestias cum enim laboriosam praesentium consectetur, sapiente unde optio illum repellat officia esse aut molestiae? Quae, nam?</p>
+</body>
+</html>
